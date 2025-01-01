2114705514e7b366a32fff9db37ddba57c25d2f4
Monzurul1988 <121655899+Monzurul1988@users.noreply.github.com>
2025-01-01 23:22:34 +0600

    Add files via upload

---
+++ 01.dart
@@ -0,0 +1,10 @@
+void triangle(double base,double height) {
+    // Calculate the area of the triangle
+  double area = 0.5 * base * height;
+  print('The area of the triangle is: $area');
+}
+
+void main() {
+  // Call the triangle function
+  triangle(10.5,3.2);
+}
